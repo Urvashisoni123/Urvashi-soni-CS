a8414d5e2712c5128c8b6d1c9bbd3f4397738a78
Urvashi Soni <urvashisoni.us2002@gmail.com>
2022-10-17 10:59:09 +0530

    day 21

--- day 20_treadmil_gui.py
+++ day 20_treadmil_gui.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tkinter as ttk
+from PIL import ImageTk,Image
 
 data=pd.read_csv('treadmil-users.csv')
 
@@ -35,8 +36,8 @@ ttk.OptionMenu(app,col1,*values).place(x=100,y=40)
 col2= ttk.Variable(app)
 values=['Select','Product','Age','Gender']
 col2.set(values[0])
-ttk.Label(app,text = 'Column 2 ').place(x=150,y=80)
-ttk.OptionMenu(app,col2,*values).place(x=150,y=110) 
+ttk.Label(app,text = 'Column 2 ').place(x=100,y=80)
+ttk.OptionMenu(app,col2,*values).place(x=100,y=110) 
 
 
 
@@ -44,11 +45,53 @@ ttk.OptionMenu(app,col2,*values).place(x=150,y=110)
 col3= ttk.Variable(app)
 values=['Select','Product','Age','Gender']
 col3.set(values[0])
-ttk.Label(app,text = 'Column 1 ').place(x=150,y=150)
-ttk.OptionMenu(app,col3,*values).place(x=150,y=180) 
+ttk.Label(app,text = 'Column 1 ').place(x=100,y=150)
+ttk.OptionMenu(app,col3,*values).place(x=100,y=180) 
+
+
+#canvas
+cnv = ttk.Canvas(app,width=200,height=200)
+cnv.place(x=200,y=100)
+
+# Label
+result = ttk.Variable(app)
+ttk.Label(app,textvariable=result).place(x=300,y=300)
 
 def show():
-     print(graphs.get())
+    global img
+    global cnv
+
+    column1 = col1.get()
+    column2 = col2.get()
+    column3 = col3.get()
+    g=graphs.get()
+
+    if 'col1' in g:
+        if column1 == 'Select':
+            result.set('column1 will be selected')
+            return
+
+    if 'col2' in g:
+        if column2 == 'Select':
+            result.set('column2 will be selected')
+            return
+
+    if 'col3' in g:
+        if column3 == 'Select':
+            result.set('column3 will be selected')
+            return        
+
+    fig=plt.figure(figsize=(5,2))
+    eval(graphs.get().format(col1 = column1,col2 = column2, col3 = column3))
+    fig.savefig('graph.png')
+    img = ImageTk.PhotoImage(
+        Image.open('graph.png').resize((200,100))
+    )
+    
+    cnv.create_image(0,0,anchor=ttk.NW,image = img)
+    
+    plt.show()
+     #print(graphs.get())
 
 ttk.Button(app,text='Show',command= show).place(x=400,y=10)
 
